7cfe11f2a4192d502c03eac1feb0af9ae7e0f49a
Osca07 <oscartorrisi72@gmail.com>
2025-10-30 10:04:15 +0100

    ciaoooo

---
+++ src/main/java/org/example/Cibo.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Cibo {
+    private String nome;
+    private float costo;
+
+    public Cibo(String nome,float costo) {
+        this.nome = nome;
+        this.costo=costo;
+    }
+}
